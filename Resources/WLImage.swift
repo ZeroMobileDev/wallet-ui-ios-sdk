7bca216094197cd8a3dc7f1790db11ae496e2dad
tahacansizoglu <taha.cansizoglu@dgpaysit.com>
2024-10-30 18:11:37 +0300

    version 1.2.9

--- Resources/WLImage.swift
+++ Resources/WLImage.swift
@@ -16,15 +16,13 @@ struct WLImage {
         self.source = .image(image)
     }
     
-   
-    
-    
     var asImage: Image {
         switch source {
         case .assetName(let name):
             if let bundlePath = Bundle.main.path(forResource: "WalletUISdkResources", ofType: "bundle"),
-               let bundle = Bundle(path: bundlePath) {
-               return Image(name, bundle: bundle)
+               let bundle = Bundle(path: bundlePath)
+            {
+                return Image(name, bundle: bundle)
             }
             return Image(name, bundle: WLResourcesBundle.current)
         case .image(let image):
@@ -33,8 +31,7 @@ struct WLImage {
     }
     
     func changeColor(_ color: Color) -> WLImage {
-        var newImage = self.asImage.renderingMode(.template).foregroundColor(color)
+        var newImage = asImage.renderingMode(.template).foregroundColor(color)
         return WLImage(image: newImage as! Image)
     }
-    
 }
