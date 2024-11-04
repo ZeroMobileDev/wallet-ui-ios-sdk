23c2526bf63794718bbc2c28a32698d8253adf42
tahacansizoglu <taha.cansizoglu@dgpaysit.com>
2024-11-04 16:16:48 +0300

    version 1.2.9.13

--- Frameworks/WalletUISdk.framework/Headers/WalletUiSdk.h
+++ Frameworks/WalletUISdk.framework/Headers/WalletUiSdk.h
@@ -8,11 +8,11 @@ FOUNDATION_EXPORT const unsigned char WalletUiVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <WalletUi/PublicHeader.h>
 
-//#import "MfsIOSLibrary.h"
-//#import "MfsCard.h"
-//#import "MfsCheckbox.h"
-//#import "MfsResponse.h"
-//#import "MfsTextField.h"
-//#import "MfsWebView.h"
+#import "MfsIOSLibrary.h"
+#import "MfsCard.h"
+#import "MfsCheckbox.h"
+#import "MfsResponse.h"
+#import "MfsTextField.h"
+#import "MfsWebView.h"
 
-#import "MasterpassHead.h"
+
